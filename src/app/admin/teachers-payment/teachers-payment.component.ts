a2a16c7a65f1df8ae81f6324052f41cf08c1b5cf
Bazilkoolath <bazilkoolath7777@gmail.com>
2023-09-25 23:16:02 +0530

    fix sidbar

--- src/app/admin/teachers-payment/teachers-payment.component.ts
+++ src/app/admin/teachers-payment/teachers-payment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddPaymentComponent } from 'src/app/shared/popup/add-payment/add-payment.component';
 
 @Component({
   selector: 'app-teachers-payment',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teachers-payment.component.css']
 })
 export class TeachersPaymentComponent implements OnInit {
+  teacherfee:any[]=[
+    {
+      id:1,
+      name:"teacher1",
+      Paid_Amount:"0548448",
+      course:"sd",
+      Payment_Status:"paid",
+    },{
+      id:2,
+      name:"teacher3",
+      Paid_Amount:"0548448",
+      course:"sd",
+      Payment_Status:"paid",
+    },{
+      id:3,
+      name:"teacher3",
+      Paid_Amount:"0548448",
+      course:"sd",
+      Payment_Status:"no",
+    }
+  ]
 
-  constructor() { }
+  constructor(
+    private _dialog:MatDialog,
+  ) { }
 
   ngOnInit(): void {
   }
+  addPayment(){
+    let dialogRef = this._dialog.open(AddPaymentComponent, {
+      width: '400px',
+      height: '100%',
+      data: {},
+      position: {
+        top: '0px',
+        right: '0px',
+      },
+      // enterAnimationDuration: '500ms',
+      direction: 'ltr',
+      panelClass: "side-popup"
+    });
 
+}
 }
