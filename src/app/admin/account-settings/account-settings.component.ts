b48b410cf1f134515f0da718e09474a765d1affc
Bazilkoolath <bazilkoolath7777@gmail.com>
2023-11-06 01:07:41 +0530

    add

--- src/app/admin/account-settings/account-settings.component.ts
+++ src/app/admin/account-settings/account-settings.component.ts
@@ -68,4 +68,15 @@ get f() {
         },
       })
   }
+  nameProfileImg(name: string) {
+    let spaceIndex =0
+    if(name?.includes(" ")){
+      spaceIndex = name?.indexOf(" ")
+    }
+    if (spaceIndex > 1) {
+      return name.slice(0, 1).toUpperCase() + name.slice(spaceIndex + 1, spaceIndex + 2).toUpperCase() || name.slice(1, 2).toUpperCase()
+    } else {
+      return name?.slice(0, 2).toUpperCase()
+    }
+  }
 }
